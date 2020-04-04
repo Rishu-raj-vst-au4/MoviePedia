7dca82650e2b5ce304fc443fb42c8a9c6a6a1b87
rishu <54232477+Rishu-raj-vst-au4@users.noreply.github.com>
2020-04-04 22:18:09 +0530

    Updates

---
+++ precache-manifest.fe5fbdda9c4344128ac2b40649f27780.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9182f1773d7e1e9f5dcda2159a747665",
+    "url": "/MoviePedia/index.html"
+  },
+  {
+    "revision": "5f2ff16da7e5b0e7dbd4",
+    "url": "/MoviePedia/static/css/main.d96254e6.chunk.css"
+  },
+  {
+    "revision": "70d4774dc6104ef41fce",
+    "url": "/MoviePedia/static/js/2.b0f535c9.chunk.js"
+  },
+  {
+    "revision": "f32951a60a4b2bee83ce34cafb3d9886",
+    "url": "/MoviePedia/static/js/2.b0f535c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2ff16da7e5b0e7dbd4",
+    "url": "/MoviePedia/static/js/main.0700fc37.chunk.js"
+  },
+  {
+    "revision": "a197e22993abca6871f1",
+    "url": "/MoviePedia/static/js/runtime-main.a3662d88.js"
+  },
+  {
+    "revision": "3d8064758e54ec662e076b6ca54aa90e",
+    "url": "/MoviePedia/static/media/spinner.3d806475.gif"
+  }
+]);
